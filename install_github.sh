8568e3af18295096f8b69327b6b177bb0a03517b
UKHazzie <richardnharrison@btinternet.com>
2024-07-23 15:58:28 +0100

    Create install_github.sh

---
+++ install_github.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt update
+sudo apt-get install git
+
+#then add
+#git config --global user.name "Richard Harrison"
+#git config --global user.email "richardnharrison@btinternet.com"
