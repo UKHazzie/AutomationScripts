a7a92af7b9e2eea3ec28a22919783eb3a1f478f0
UKHazzie <richardnharrison@btinternet.com>
2024-07-23 16:02:38 +0100

    Update install_github.sh

--- install_github.sh
+++ install_github.sh
@@ -5,3 +5,14 @@ sudo apt-get install git
 #then add
 #git config --global user.name "Richard Harrison"
 #git config --global user.email "richardnharrison@btinternet.com"
+
+#create ssh
+#press return on passphrase (for examples)
+#ssh-keygen -t rsa -f ~/.ssh/id_rsa <<< y
+
+#Take key and paste it into github ssh keys section
+#cat ~/.ssh/id_rsa.pub
+
+#once done conenct up to github
+#ssh -T git@github.com
+#accept fingerprint.
